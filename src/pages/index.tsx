ba9ef04c298bb56ede3a0977d2bf6f81865c3379
doxanocap <eldos.kadirbekov.2021@gmail.com>
2023-06-23 20:46:58 +0600

    responsive fix 0.1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,19 +7,17 @@ const Home = () => (
     <Header />
     <main className="flex flex-col items-center min-h-screen bg-cwhite">
       {/* BODY 1 */}
-      <div className="flex bg-yellow-300 h-[800px] sm:w-[1080px] w-[1080px] lg:w-[1410px] text-center px-[30px] items-center justify-center">
-        {/* <div className="flex w-[650px] ">
+      <div className="flex h-[800px] sm:w-[1080px] w-[1080px] lg:w-[1410px] text-center px-[30px] items-center justify-center">
+        <div className="flex w-[650px] ">
           <div className="flex flex-col justify-center font-black text-left text-black text-h1">
-            <p className="">
-              Transform Your Business with us
-            </p>
+            <p className="">Transform Your Business with us</p>
             <div className="flex">
               <p className="text-cblue">Witr.kz </p>
               <p className="ml-3"> - Your Trusted IT Partner </p>
             </div>
           </div>
         </div>
-        <Graphics /> */}
+        <Graphics />
       </div>
     </main>
     <Footer />
